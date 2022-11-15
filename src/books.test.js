8a3873584eb74acbff2130d157d780d2e8dd12d3
Tommy Han <kattyntommy@gmail.com>
2022-11-15 20:39:47 +0000

    refactor

--- src/books.test.js
+++ src/books.test.js
@@ -1,33 +1,33 @@
 import BooksPresenter from "./Books/BooksPresenter";
 import httpGateway from "./Shared/HttpGateway";
 
-httpGateway.get = jest.fn().mockImplementation(() => {
-  return {
-    success: true,
-    result: [
-      {
-        bookId: 18051,
-        name: "Wind in the willows",
-        ownerId: "tommy.han.cs@gmail.com",
-        author: "Kenneth Graeme",
-      },
-      {
-        bookId: 18061,
-        name: "I, Robot",
-        ownerId: "tommy.han.cs@gmail.com",
-        author: "Isaac Asimov",
-      },
-      {
-        bookId: 18071,
-        name: "The Hobbit",
-        ownerId: "tommy.han.cs@gmail.com",
-        author: "Jrr Tolkein",
-      },
-    ],
-  };
-});
-
 it("should confirm that we can load 3 model books and hit backend", async () => {
+  httpGateway.get = jest.fn().mockImplementation(() => {
+    return {
+      success: true,
+      result: [
+        {
+          bookId: 18051,
+          name: "Wind in the willows",
+          ownerId: "tommy.han.cs@gmail.com",
+          author: "Kenneth Graeme",
+        },
+        {
+          bookId: 18061,
+          name: "I, Robot",
+          ownerId: "tommy.han.cs@gmail.com",
+          author: "Isaac Asimov",
+        },
+        {
+          bookId: 18071,
+          name: "The Hobbit",
+          ownerId: "tommy.han.cs@gmail.com",
+          author: "Jrr Tolkein",
+        },
+      ],
+    };
+  });
+
   let viewModel = null;
   let booksPresenter = new BooksPresenter();
   await booksPresenter.load((generatedViewModel) => {
@@ -48,6 +48,14 @@ it("should confirm that we can load 1 book and hit backend", async () => {
     name: "To Kill a Mocking Bird",
     author: "Andy Warhol",
   };
+
+  httpGateway.post = jest.fn().mockImplementation(() => {
+    return {
+      success: true,
+      result: "book created",
+    };
+  });
+
   let booksPresenter = new BooksPresenter();
   const response = await booksPresenter.addBook(requestBodyDto);
 
@@ -55,4 +63,5 @@ it("should confirm that we can load 1 book and hit backend", async () => {
     "https://api.logicroom.co/api/tommy.han.cs@gmail.com/books"
   );
   expect(response.success).toBe(true);
+  expect(response.result).toBe("book created");
 });
